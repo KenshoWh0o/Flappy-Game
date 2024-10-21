603d8ee8dca0c475f25cec7b4fb335b1081656ed
Kensho <ari.felix0612@gmail.com>
2024-10-21 20:16:37 +0200

    Flappy-game

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+    public float deadZoneBot = -14;
+    public float deadZoneTop = 14;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+        
+        if (transform.position.y < deadZoneBot || transform.position.y > deadZoneTop)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
